f0f7ed36fd4df4c05acfb50448c2aa23b150abbc
schetan9075 <patilaradhya680@gmail.com>
2022-06-13 10:14:15 +0530

    upload mathmodule

---
+++ chetu_mathsmodule.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 19:03:48 2022
+
+@author: chetu
+"""
+
+import math
+x= 5
+print(math.ceil(x))
+print(math.fabs(x))
+print(math.factorial(x))
+print(math.floor(x))
+print(math.sqrt(x))
+
+a=[1,20,30,40,50]
+print(math.fsum(a))
+
