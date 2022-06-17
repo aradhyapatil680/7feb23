a3e244dd0b357c39fe178f137acf26a121474e9d
schetan9075 <patilaradhya680@gmail.com>
2022-06-17 16:20:31 +0530

    add new file

---
+++ filehandling.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 10:56:29 2022
+
+@author: chetu
+"""
+
+with open("hello.txt",'w') as f:
+    f.write("welcome to\n")
+    f.write(" our \n\n")
+    f.write("home\n")
